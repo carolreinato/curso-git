30ede67e60d7e68306e43b02846bc567e40aae49
Carol <cr.reinato@gmail.com>
2019-07-10 17:12:48 -0300

    Mensagaem Ola mundo

---
+++ Teste_git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
